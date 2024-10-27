cbc524e4019e7c3ecc92ca29179dff369d6f7d5d
Daniel de Haan <u7623919@anu.edu.au>
2024-10-27 21:05:03 +1100

    Corrected key reference in setSaveShowShortsItems. Made consistent with other "set" functions.

--- app/src/main/java/org/schabi/newpipe/local/feed/FeedViewModel.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/FeedViewModel.kt
@@ -156,9 +156,9 @@ class FeedViewModel(
     fun getShowFutureItemsFromPreferences() = getShowFutureItemsFromPreferences(application)
 
     fun setSaveShowShortsItems(showShortsItems: Boolean) {
+        this.showFutureItems.onNext(showShortsItems)
         PreferenceManager.getDefaultSharedPreferences(application).edit {
-
-            this.putBoolean(application.getString(R.string.feed_show_future_items_key), showShortsItems)
+            this.putBoolean(application.getString(R.string.feed_show_shorts_items_key), showShortsItems)
             this.apply()
         }
     }
